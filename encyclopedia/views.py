5479ceb511b653231762782d88492a94aa1beaec
Carlos E Alford <10241920+CarlosEAM@users.noreply.github.com>
2023-01-23 19:50:20 +0000

    Navigation to 'wiki/<ENTRY_NAME>' displays the Markdown as HTML page if it exists

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-from . import util
+from . import util, mk2html
 
 
 def index(request):
@@ -8,3 +9,12 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, entry):
+    mk_doc = util.get_entry(entry)
+    if (mk_doc == 'NONE'):
+        return HttpResponseRedirect('PAGE WAS NOT FOUND')
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "html_entry": mk2html.parse_file(mk_doc)
+        })

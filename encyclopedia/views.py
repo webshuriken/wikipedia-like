a9e3e0ce5a75febfbe05b75da855c468b3f8c2ca
Carlos E Alford <10241920+CarlosEAM@users.noreply.github.com>
2023-02-17 19:57:18 +0000

    feat: new-page, has a view, urls, and template setup

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,8 +1,14 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django import forms
+from django.urls import reverse
 
 from . import util, mk2html
 
+class CreateNewPage(forms.Form):
+    title = forms.CharField(label="Title", strip=True)
+    content = forms.CharField(widget=forms.Textarea, label="Content", strip=True)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -11,9 +17,8 @@ def index(request):
 
 def entry(request, entry):
     mk_doc = util.get_entry(entry)
-    print(f'ENTRY FOUND: {type(mk_doc)}')
     if (mk_doc == None):
-        return render(request, 'encyclopedia/404-entry.html')
+        return render(request, "encyclopedia/404-entry.html")
     else:
         return render(request, "encyclopedia/entry.html", {
             "entry_title": entry,
@@ -22,3 +27,28 @@ def entry(request, entry):
 
 def entry_not_found(request):
     return render(request, "encyclopedia/404-entry.html")
+
+def new_page(request):
+    # catch form submission
+    if request.method == 'POST':
+        # create new form with user submitted data
+        form = CreateNewPage(request.POST)
+        # does the submitted form follow the rules
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            # prepend the title to the content
+            content = f"# {title}\n\n{content}"
+            # save the new entry to file
+            util.save_entry(title, content)
+            return HttpResponseRedirect(reverse(f"encyclopedia:entry", args=[title]))
+
+        else:
+            return render(request, "encyclopedia/new-page.html", {
+                "form": form
+            })
+
+    # fresh request, fresh form
+    return render(request, "encyclopedia/new-page.html", {
+        "form": CreateNewPage()
+    })
